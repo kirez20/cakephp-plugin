ebe5478650bc92a55d5d7531cb94ca3868255541
KireZ <kiril.zvezdakoski@gmail.com>
2021-01-07 02:23:26 +0100

    bin/cake bake controller --plugin ContactManager Contacts

---
+++ plugins/ContactManager/tests/TestCase/Controller/ContactsControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace ContactManager\Test\TestCase\Controller;
+
+use Cake\TestSuite\IntegrationTestTrait;
+use Cake\TestSuite\TestCase;
+use ContactManager\Controller\ContactsController;
+
+/**
+ * ContactManager\Controller\ContactsController Test Case
+ *
+ * @uses \ContactManager\Controller\ContactsController
+ */
+class ContactsControllerTest extends TestCase
+{
+    use IntegrationTestTrait;
+
+    /**
+     * Test initial setup
+     *
+     * @return void
+     */
+    public function testInitialization()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
